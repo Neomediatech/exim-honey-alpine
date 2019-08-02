4c56241b6e12cea5218265aec8d6533ddf33b633
Dario B <feddar@users.noreply.github.com>
2019-08-02 18:53:06 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 
 touch /var/log/exim/mainlog /var/log/exim/rejectlog /var/log/exim/paniclog; chown exim:exim /var/log/exim/mainlog /var/log/exim/rejectlog /var/log/exim/paniclog
 
+mkdir -p /data/mail && chmod 777 /data/mail
+
 exim -bd
 tail -f /var/log/exim/mainlog 
-
